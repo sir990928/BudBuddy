83a20680481f2796b8a8dd6a3eaed2c83845cc46
BenEgeDeniz <benegedeniz@users.noreply.github.com>
2026-08-20 20:41:12 +0300

    chore: bump version to 1.7.0 and add localized changelogs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.benegedeniz.budsdynamiceq"
         minSdk = 31
         targetSdk = 37
-        versionCode = 9
-        versionName = "1.6.0"
+        versionCode = 10
+        versionName = "1.7.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
