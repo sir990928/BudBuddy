7259ed655c740b78b5307403fa28ad94e8ff5072
sir990928 <77320101@qq.com>
2026-09-04 20:45:37 +0800

    Add Chinese locale resources

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         versionName = "1.7.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        resourceConfigurations += listOf("en", "tr", "az", "ru")
+        resourceConfigurations += listOf("en", "tr", "az", "ru", "zh-rCN")
     }
 
     bundle {
